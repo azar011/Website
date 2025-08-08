0e873d09f109ce78f377d467f28cb691393b30af
anishfathima08 <anisfathima324@gmail.com>
2025-08-08 16:57:55 +0530

    commit

--- Frontend/src/Components/CommonComponents/Header.jsx
+++ Frontend/src/Components/CommonComponents/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
 
-export default Header
+<h1 className='text-blue'></h1>
+    </>
+  );
+};
+
+export default Header;
