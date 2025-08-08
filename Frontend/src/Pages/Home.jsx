d48ca1d6289ff8ad656127f780ed1cf678caace3
anishfathima08 <anisfathima324@gmail.com>
2025-08-08 16:06:55 +0530

    commit

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../Components/CommonComponents/Header'
+import Footer from '../Components/CommonComponents/Footer'
+import HeroSection from '../Components/HomePageComponents/HeroSection'
+import WhyChooseUs from '../Components/HomePageComponents/WhyChooseUs'
+import AboutSection from '../Components/HomePageComponents/AboutSection'
+import CourseSection from '../Components/HomePageComponents/CourseSection'
+import Testimonial from '../Components/HomePageComponents/Testimonial'
+import NeedHelp from '../Components/HomePageComponents/NeedHelp'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <CourseSection />
+      <WhyChooseUs />
+      <Testimonial />
+      <NeedHelp />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
